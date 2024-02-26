4b64e7c5366b31a1710da47aad863944fab6c192
Victor Matheus <victormatheus507@gmail.com>
2024-02-26 10:01:41 -0300

    criando funcao para ler o arquivo

--- avaliativa1/ex02/ex02.py
+++ avaliativa1/ex02/ex02.py
@@ -1,20 +1,31 @@
 import os 
 
 while True:
+  nomeDoArquivo = input('DIgite o nome do arquivo: ')
+
+  def ler_arquivo(nome_arquivo):
     try:
-        nomeDoArquivo = input('DIgite o nome do arquivo: ')
+      caminhoArquivo = os.path.abspath(f'avaliativa1\ex01\{nome_arquivo}.txt')
 
-        caminhoArquivo = os.path.abspath(f'avaliativa1\ex01\{nomeDoArquivo}.txt')
-        with open(caminhoArquivo, 'r', encoding='utf-8') as arquivoSelecionado:
-            conteudoDoArquivo = arquivoSelecionado.read()
-        print(conteudoDoArquivo)
-
-        break
+      with open(caminhoArquivo, 'r', encoding='utf-8') as arquivoSelecionado:
+          listaValores = [linha.strip() for linha in arquivoSelecionado]
+          return True, listaValores
 
     except TypeError:
         print("\nERROR: Digite um valor válido de arquivo")
-        continue
+        return False, None
+
     except FileNotFoundError:
         print(f"\nERROR: O arquivo não foi encontrado")
+        return False, None
+
     except Exception as e:
         print(f"\nERROR: {e}")
+        return False, None
+
+  arquivoLidoComSucessso, listaValores = ler_arquivo(nomeDoArquivo)
+  
+  if arquivoLidoComSucessso:
+    print(arquivoLidoComSucessso, listaValores)
+    break
+
