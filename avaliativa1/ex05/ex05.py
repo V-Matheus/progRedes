58dec602df9d30d98b154cf7ac111df08bf93dac
Victor Matheus <victormatheus507@gmail.com>
2024-03-01 15:48:41 -0300

    criptografando bit a bit

--- avaliativa1/ex05/ex05.py
+++ avaliativa1/ex05/ex05.py
@@ -6,4 +6,14 @@ def cifra_xor(file_origem, palavraPasse, file_destino):
 
     dados_criptografados = []
 
+    for i, byte in enumerate(dados):
+        indice_palavraPasse = i % len(palavraPasse_bytes)
 
+        byte_criptografado = byte ^ palavraPasse_bytes[indice_palavraPasse]
+
+        dados_criptografados.append(byte_criptografado)
+
+    with open(file_destino, 'xb') as destino_arquivo:
+        destino_arquivo.write(bytes(dados_criptografados))
+
+    print("Arquivo criptografado com sucesso!")
