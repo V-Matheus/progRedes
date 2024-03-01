aa43766c7d2207b8f6cafe77a5a02a0d6171a00b
Victor Matheus <victormatheus507@gmail.com>
2024-02-29 22:46:53 -0300

    finaliando jogo

--- avaliativa1/ex04/ex04.py
+++ avaliativa1/ex04/ex04.py
@@ -1,15 +1,16 @@
 from funcoes import *
 
 palavras = lerArquivo()
-# palavraSorteada = sortearPalavra(palavras)
-palavraSorteada = list('python')
+palavraSorteada = sortearPalavra(palavras)
+palavraSorteada_string = ''.join(palavraSorteada)
+tentativas = 1
 
 while True:
 
-  tentativas = 1
   chute = input('Digite uma palavra entre 5 e 8 letras: ').lower()
 
   print(f'A palvra tem {len(palavraSorteada)} letras')
+  print(f'Você tem {6 - tentativas} tentativas restantes')
 
   for index, letra in enumerate(chute):
     if letra in palavraSorteada:
@@ -20,3 +21,12 @@ while True:
     else:
       print(f'\033[1;31m{letra}\033[0m', end=' ')
   print('')
+
+  if chute == palavraSorteada_string:
+    print(f'Parabéns você ganhou :) \n Você encontrou a palavra com {tentativas} tentativa(s)')
+
+  tentativas += 1
+
+  if tentativas == 6:
+    print(f'Você perdeu, acabou as suas tentativas :( \n A palavra secreta era {palavraSorteada_string}')
+    break
