23377ee4f0f1d311d07ceca9918affd601b01f3f
Victor Matheus <victormatheus507@gmail.com>
2024-02-29 23:05:07 -0300

    tratando excecoes

--- avaliativa1/ex04/ex04.py
+++ avaliativa1/ex04/ex04.py
@@ -1,32 +1,46 @@
 from funcoes import *
 
-palavras = lerArquivo()
-palavraSorteada = sortearPalavra(palavras)
-palavraSorteada_string = ''.join(palavraSorteada)
-tentativas = 1
+try:
+  palavras = lerArquivo()
+  palavraSorteada = sortearPalavra(palavras)
+  palavraSorteada_string = ''.join(palavraSorteada)
+  tentativas = 1
 
-while True:
+  print(f'A palavra tem {len(palavraSorteada)} letras')
 
-  chute = input('Digite uma palavra entre 5 e 8 letras: ').lower()
+  while True:
+      chute = input('Digite uma palavra entre 5 e 8 letras: ').lower()
 
-  print(f'A palvra tem {len(palavraSorteada)} letras')
-  print(f'Você tem {6 - tentativas} tentativas restantes')
+      if any(char.isdigit() for char in chute):
+          print("Entrada inválida. Digite apenas letras.")
+          continue 
 
-  for index, letra in enumerate(chute):
-    if letra in palavraSorteada:
-      if palavraSorteada[index] == letra:
-        print(f'\033[93m{letra}\033[0m', end=' ')
-      else:
-        print(f'\033[92m{letra}\033[0m', end=' ')
-    else:
-      print(f'\033[1;31m{letra}\033[0m', end=' ')
-  print('')
+      if (not (5 <= len(chute)) <= 8) or (len(chute) > len(palavraSorteada)):
+          print(f'A palavra tem {len(palavraSorteada)} letras')
+          continue
 
-  if chute == palavraSorteada_string:
-    print(f'Parabéns você ganhou :) \n Você encontrou a palavra com {tentativas} tentativa(s)')
+      print(f'Você tem {6 - tentativas} tentativas restantes')
 
-  tentativas += 1
+      for index, letra in enumerate(chute):
+          if letra in palavraSorteada:
+              if palavraSorteada[index] == letra:
+                  print(f'\033[93m{letra}\033[0m', end=' ')
+              else:
+                  print(f'\033[92m{letra}\033[0m', end=' ')
+          else:
+              print(f'\033[1;31m{letra}\033[0m', end=' ')
+      print('')
 
-  if tentativas == 6:
-    print(f'Você perdeu, acabou as suas tentativas :( \n A palavra secreta era {palavraSorteada_string}')
-    break
+      if chute == palavraSorteada_string:
+          print(f'Parabéns você ganhou :) \n Você encontrou a palavra com {tentativas} tentativa(s)')
+          break
+
+      tentativas += 1
+
+      if tentativas == 6:
+          print(f'Você perdeu, acabou as suas tentativas :( \n A palavra secreta era {palavraSorteada_string}')
+          break
+
+
+except Exception as e:
+    print(f"Erro geral: {e}")
