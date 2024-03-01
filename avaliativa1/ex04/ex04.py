ec0c7d54781f1dda02c47befdfb5300f43f18e01
Victor Matheus <victormatheus507@gmail.com>
2024-02-29 22:14:41 -0300

    verificando as letras e posicoes

--- avaliativa1/ex04/ex04.py
+++ avaliativa1/ex04/ex04.py
@@ -1,6 +1,20 @@
 from funcoes import *
 
-arquivo = input('Digite o nome do arquivo: ')
+palavras = lerArquivo()
+# palavraSorteada = sortearPalavra(palavras)
+palavraSorteada = list('python')
 
-palavras = lerArquivo(arquivo)
-print(sortearPalavra(palavras))
+while True:
+
+  tentativas = 1
+  chute = input('Digite uma palavra entre 5 e 8 letras: ').lower()
+
+  for index, letra in enumerate(chute):
+    if letra in palavraSorteada:
+      if palavraSorteada[index] == letra:
+        print(f'A letra "{letra}" está correta')
+      else:
+        print(f'A letra "{letra}" existe, porém não está no local correto')
+    else:
+      print(f'Não existe a letra "{letra}" na palavra secreta')
+  print('')
--- avaliativa1/ex04/funcoes.py
+++ avaliativa1/ex04/funcoes.py
@@ -1,15 +1,11 @@
 import os, random
 
-def lerArquivo(nome_arquivo):
-    try:
-        caminho_arquivo = os.path.join(os.path.dirname(__file__), nome_arquivo + '.txt')
-        with open(caminho_arquivo, 'r') as arquivo:
-            palavras = [palavra.strip() for palavra in arquivo.readlines() if 5 <= len(palavra.strip()) <= 8]
-        return palavras
-    except FileNotFoundError:
-        print(f"Arquivo '{nome_arquivo}' não encontrado.")
-        return None
+def lerArquivo():
+    caminho_arquivo = os.path.join(os.path.dirname(__file__), 'palavras.txt')
+    with open(caminho_arquivo, 'r') as arquivo:
+        palavras = [palavra.strip() for palavra in arquivo.readlines() if 5 <= len(palavra.strip()) <= 8]
+    return palavras
 
 
 def sortearPalavra(listaPalavras):
-    return listaPalavras[random.randint(1, len(listaPalavras))]
+    return list(listaPalavras[random.randint(1, len(listaPalavras))].lower())
