79e1f41dec75af28bbf5d40c90d8b6063bab7c9f
Victor Matheus <victormatheus507@gmail.com>
2024-03-01 16:06:25 -0300

    criando excecoes

--- avaliativa1/ex05/ex05.py
+++ avaliativa1/ex05/ex05.py
@@ -1,19 +1,27 @@
 def cifra_xor(file_origem, palavraPasse, file_destino):
-    with open(file_origem, 'rb') as origem_arquivo:
-        dados = origem_arquivo.read()
+    try:
+        with open(file_origem, 'rb') as origem_arquivo:
+            dados = origem_arquivo.read()
 
-    palavraPasse_bytes = palavraPasse.encode('utf-8')
+        palavraPasse_bytes = palavraPasse.encode('utf-8')
 
-    dados_criptografados = []
+        dados_criptografados = []
 
-    for i, byte in enumerate(dados):
-        indice_palavraPasse = i % len(palavraPasse_bytes)
+        for i, byte in enumerate(dados):
+            indice_palavraPasse = i % len(palavraPasse_bytes)
 
-        byte_criptografado = byte ^ palavraPasse_bytes[indice_palavraPasse]
+            byte_criptografado = byte ^ palavraPasse_bytes[indice_palavraPasse]
 
-        dados_criptografados.append(byte_criptografado)
+            dados_criptografados.append(byte_criptografado)
 
-    with open(file_destino, 'xb') as destino_arquivo:
-        destino_arquivo.write(bytes(dados_criptografados))
+        with open(file_destino, 'xb') as destino_arquivo:
+            destino_arquivo.write(bytes(dados_criptografados))
 
-    print("Arquivo criptografado com sucesso!")
+        print("Arquivo criptografado com sucesso!")
+
+    except FileNotFoundError:
+        print("Arquivo de origem não encontrado.")
+    except FileExistsError:
+        print("O arquivo de destino já existe. Escolha um nome diferente.")
+    except Exception as e:
+        print(f"Erro: {e}")
