9362ac2e9678398a694631d16dae88fab101f2ef
V-Matheus <victormatheus507@gmail.com>
2024-03-04 16:13:36 -0300

    melhorando código

--- avaliativa1/ex03/ex03.py
+++ avaliativa1/ex03/ex03.py
@@ -5,18 +5,20 @@ def findNonce(dataToHash, bitsToBeZero):
   start_time = time.time()
   nonce = 0
 
-  while True:
+  for nonce in range(2**32):
 
         data = struct.pack('<Q', nonce) + dataToHash
 
         hash_result = hashlib.sha256(data).digest()
 
         if int.from_bytes(hash_result[:bitsToBeZero // 8], byteorder='big') == 0:
-            end_time = time.time()  # Registrar o tempo de término
+            end_time = time.time()
             tempo = end_time - start_time
             return nonce, tempo
         
         nonce += 1
+
+  return None, None
 
 transacoes = input('Digite um texto para validação: ').encode('utf-8')
 bitsZero = int(input('Quantidade de Bits 0: '))
