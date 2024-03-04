e368c3fdf7274d7899a7c3a91ed7904405f6777b
V-Matheus <victormatheus507@gmail.com>
2024-03-04 15:36:58 -0300

    declarando variaveis

--- avaliativa1/ex03/ex03.py
+++ avaliativa1/ex03/ex03.py
@@ -1,1 +1,20 @@
-#teste
+import hashlib, struct, time
+
+def findNonce(dataToHash, bitsToBeZero):
+
+  nonce = None
+  tempo = None
+
+  return nonce, tempo
+
+
+transacoes = input('Digite um texto para validação: ').encode('utf-8')
+bitsZero = int(input('Quantidade de Bits 0: '))
+
+isNonce, intTempo = findNonce(transacoes, bitsZero)
+
+if isNonce:
+  print(isNonce, intTempo)
+
+else:
+  print('Não foi encontrado NONCE...')
