510c99477b39ec1cca9eb4ff2f8434e2b1e3c8c6
V-Matheus <victormatheus507@gmail.com>
2024-03-25 18:07:06 -0300

    lendo cabeçalhos dos pacotes

--- avaliativa2/avaliativa.py
+++ avaliativa2/avaliativa.py
@@ -2,3 +2,14 @@ from funcoes import *
 
 nomeArquivoTcp = input("Digite o nome do arquivo capturado pelo tcpdump: ")
 lerCabecalho(nomeArquivoTcp)
+
+with open(nomeArquivoTcp, 'rb') as arquivo:
+  arquivo.read(24)  # O cabeçalho do arquivo tem 24 bytes de tamanho
+
+  while True:
+    try:
+      header_length = ler_pacote_ip(arquivo)
+      arquivo.read(header_length)  # Ler apenas o cabeçalho
+
+    except struct.error:
+      break
