32e020102b5003b868a06c3cc9af2fac61d86507
V-Matheus <victormatheus507@gmail.com>
2024-08-14 16:07:54 -0300

    estudo dirigido 04

--- estudos_dirigidos/04/client/client_side.py
+++ estudos_dirigidos/04/client/client_side.py
@@ -4,32 +4,36 @@ SERVER = '127.0.0.1'
 PORT   = 31435
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.settimeout(20) 
-#sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF, 25000)
+sock.settimeout(5) 
 
 while True:
     strNomeArq = input('Nome do arquivo a fazer download: ')
     sock.sendto(strNomeArq.encode('utf-8'), (SERVER, PORT))
 
     try:
+        data, addr = sock.recvfrom(4096)
+        file_size = int(data.decode('utf-8'))
+        print(f"Tamanho do arquivo recebido: {file_size} bytes")
+        
         fd = open(strNomeArq, 'wb')
-        tam = 0
-        while True:
+        received_size = 0 
+
+        while received_size < file_size:
             data, addr = sock.recvfrom(4096)
 
             if not data: 
                 break
 
-            tam += len(data)
-            print (f'Recebi {len(data) / tam} bytes')
+            received_size += len(data)
+            print (f'Recebi {len(data) / received_size} bytes')
             fd.write(data)
+        print('Recepção do arquivo concluída.')
 
     except socket.timeout:
         print('Tempo de espera esgotado')
-        fd.close()
-        break
 
     finally:
         fd.close()
+        sock.close()
+        break
 
-sock.close()
